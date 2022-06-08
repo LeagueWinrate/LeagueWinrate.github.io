11741842a36c8b7126c92b6211230e0dcf115f66
Adrian Chan <amchan@ncsu.edu>
2022-06-07 21:22:28 -0400

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameInput } from './name-input/name-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameInput
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/name-input/name-input.component.ts
@@ -0,0 +1,49 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'lw-names',
+    templateUrl: './name-input.components.html',
+    styleUrls: ['./name-input.components.css']
+})
+export class NameInput {
+    nameInput = '';
+    names: string[] = [];
+    allyChampions: string[] = ['','','','',''];
+    enemyChampions: string[] = ['','','','','']
+    estimateClicked = false;
+
+    addNames(): void {
+        this.names = this.nameInput.split('\n')
+        this.names = this.names.map(e => e.replace(" joined the lobby", ""))
+    }
+
+    getValue(): void {
+        this.names.forEach((player, id) => {
+            let allyChampInput = document.querySelector("#"+player) as HTMLInputElement;
+            let enemyChampInput = document.querySelector("#enemy"+id) as HTMLInputElement;
+            if (allyChampInput != null) {
+                this.allyChampions[id] = allyChampInput.value.replace(/\s+/g, '').toLocaleLowerCase();
+            }
+
+            if (enemyChampInput != null) {
+                this.enemyChampions[id] = enemyChampInput.value.replace(/\s+/g, '').toLocaleLowerCase();
+            }
+        })
+    }
+
+    estimateClick(): void {
+        let emptyChamp = false
+        this.allyChampions.forEach((champ) => {
+            if (champ === '') {
+                emptyChamp = true;
+            }
+        })
+        this.enemyChampions.forEach((champ) => {
+            if (champ === '') {
+                emptyChamp = true;
+            }
+        })
+        this.estimateClicked = !emptyChamp;
+        console.log(this.estimateClicked)
+    }
+}
