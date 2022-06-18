1d0e465093e8def86dd0fd3fa74e4e92c6788389
Adrian Chan <amchan@ncsu.edu>
2022-06-18 16:11:32 -0400

    website

--- src/app/riot/riot.service.ts
+++ src/app/riot/riot.service.ts
@@ -12,7 +12,7 @@ export class RiotService {
 
   getRiotAPI(data: RiotInput):Observable<any> {
 
-    const api_gateway = "https://d3dgyzfa4h.execute-api.us-east-1.amazonaws.com/beta"
+    const api_gateway = "https://1rdolqehpd.execute-api.us-east-1.amazonaws.com/winrate"
     console.log(data)
     let request = this.http.post<any>(api_gateway, JSON.stringify(data))
     return request;
