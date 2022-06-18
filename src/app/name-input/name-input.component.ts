be7a806c34130a4de9fe28b0d0199a6702890c79
Adrian Chan <amchan@ncsu.edu>
2022-06-18 17:37:12 -0400

    updated website

--- src/app/name-input/name-input.component.ts
+++ src/app/name-input/name-input.component.ts
@@ -11,10 +11,10 @@ export class NameInput {
     nameInput = '';
     names: string[] = [];
     allyChampions: string[] = [];
-    enemyChampions: string[] = []
+    enemyChampions: string[] = [];
     addNamesClicked = false;
     estimateClicked = false;
-    output = "Loading Please Wait... This will take about 30-45 seconds"
+    output = "Loading Please Wait... This will take about 5-10 seconds"
 
     constructor(private riotService: RiotService) {}
     addNames(): void {
@@ -24,6 +24,8 @@ export class NameInput {
     }
 
     getValue(): void {
+        this.allyChampions = [];
+        this.enemyChampions = [];
         for (let i = 0; i < 5; i++) {
             let allyChampInput = document.querySelector("#ally"+i) as HTMLInputElement;
             let enemyChampInput = document.querySelector("#enemy"+i) as HTMLInputElement;
@@ -40,29 +42,15 @@ export class NameInput {
     }
 
     estimateClick(): void {
+        this.getValue()
         let emptyChamp = this.allyChampions.length == 0 || this.enemyChampions.length == 0
-        let data: RiotInput = {
-            "name1": this.names[0],
-            "name2": this.names[1],
-            "name3": this.names[2],
-            "name4": this.names[3],
-            "name5": this.names[4],
-            "allyChamp1": this.allyChampions[0],
-            "allyChamp2": this.allyChampions[1],
-            "allyChamp3": this.allyChampions[2],
-            "allyChamp4": this.allyChampions[3],
-            "allyChamp5": this.allyChampions[4],
-            "enemyChamp1": this.enemyChampions[0],
-            "enemyChamp2": this.enemyChampions[1],
-            "enemyChamp3": this.enemyChampions[2],
-            "enemyChamp4": this.enemyChampions[3],
-            "enemyChamp5": this.enemyChampions[4],
-        }
         this.estimateClicked = !emptyChamp;
-        console.log("start")
+        this.output = "Loading Please Wait... This will take about 5-10 seconds"
         if (!emptyChamp) {
-            this.riotService.getRiotAPI(data).subscribe({
-                next: result => this.output = result.body,
+            this.riotService.getRiotAPI(this.names, this.allyChampions, this.enemyChampions).subscribe({
+                next: result => {
+                    this.output = result.body
+                },
                 error: err => console.log(err)
             }
             );
--- src/app/riot/riot.service.ts
+++ src/app/riot/riot.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { json } from 'stream/consumers';
@@ -10,11 +10,27 @@ export class RiotService {
 
   constructor(private http: HttpClient) { }
 
-  getRiotAPI(data: RiotInput):Observable<any> {
+  getRiotAPI(names: string[], allyChampions: string[], enemyChampions: string[]):Observable<any> {
 
+    let params = new HttpParams().set("name1", names[0])
+                                 .set("name2", names[1])
+                                 .set("name3", names[2])
+                                 .set("name4", names[3])
+                                 .set("name5", names[4])
+                                 .set("allyChampion1", allyChampions[0])
+                                 .set("allyChampion2", allyChampions[1])
+                                 .set("allyChampion3", allyChampions[2])
+                                 .set("allyChampion4", allyChampions[3])
+                                 .set("allyChampion5", allyChampions[4])
+                                 .set("enemyChampion1", enemyChampions[0])
+                                 .set("enemyChampion2", enemyChampions[1])
+                                 .set("enemyChampion3", enemyChampions[2])
+                                 .set("enemyChampion4", enemyChampions[3])
+                                 .set("enemyChampion5", enemyChampions[4])
+
+    
     const api_gateway = "https://1rdolqehpd.execute-api.us-east-1.amazonaws.com/winrate"
-    console.log(data)
-    let request = this.http.post<any>(api_gateway, JSON.stringify(data))
+    let request = this.http.get<any>(api_gateway, {params: params})
     return request;
     
   }
